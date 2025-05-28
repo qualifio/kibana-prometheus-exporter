643a165d95fdae16273491428c66a0c08f80ef60
Francois Tamigniau <francois.tamigniau@qualifio.com>
2025-05-28 10:10:23 +0200

    remove indent

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,48 +1,48 @@
-  import { IRouter } from '../../../../src/core/server';
-  import formatter  from '../../common/formatter';
-  import fetch from 'node-fetch';
-  import httpsAgent from './http_utils';
+import { IRouter } from '../../../../src/core/server';
+import formatter  from '../../common/formatter';
+import fetch from 'node-fetch';
+import httpsAgent from './http_utils';
 
-  export function defineRoutes(router: IRouter, statsURI: string) {
-    router.get(
-      {
-        path: '/_prometheus/metrics',
-        validate: false,
-        options: {
-          tags: ['access:public'],
-        },
+export function defineRoutes(router: IRouter, statsURI: string) {
+  router.get(
+    {
+      path: '/_prometheus/metrics',
+      validate: false,
+      options: {
+        tags: ['access:public'],
       },
-      async (_, request, response) => {
-        let reqHeaders = {};
-        let reqProto = request.url.protocol || 'http:';
-        let reqHost = request.url.host?.replace(/:\d+/, '') || '127.0.0.1';
-        let reqPort = request.url.port || 5601;
-        let reqUrl = `${reqProto}//${reqHost}:${reqPort}${statsURI}`;
+    },
+    async (_, request, response) => {
+      let reqHeaders = {};
+      let reqProto = request.url.protocol || 'http:';
+      let reqHost = request.url.host?.replace(/:\d+/, '') || '127.0.0.1';
+      let reqPort = request.url.port || 5601;
+      let reqUrl = `${reqProto}//${reqHost}:${reqPort}${statsURI}`;
 
-        if (request.headers !== undefined
-            && request.headers.authorization !== undefined) {
-          reqHeaders = { 'Authorization': request.headers.authorization };
-        }
-
-        const agent = reqProto === 'https:' ? httpsAgent : undefined;
-        const kbnInternalStatusRequest = await fetch(reqUrl, {
-          agent,
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/json',
-            ...reqHeaders
-          }
-        });
-
-        const kbnMetrics = await kbnInternalStatusRequest.json();
-        const promMetrics = formatter(kbnMetrics);
-
-        return response.ok({
-          headers: {
-            'Content-Type': 'text/plain',
-          },
-          body: promMetrics,
-        });
+      if (request.headers !== undefined
+          && request.headers.authorization !== undefined) {
+        reqHeaders = { 'Authorization': request.headers.authorization };
       }
-    );
-  }
+
+      const agent = reqProto === 'https:' ? httpsAgent : undefined;
+      const kbnInternalStatusRequest = await fetch(reqUrl, {
+        agent,
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          ...reqHeaders
+        }
+      });
+
+      const kbnMetrics = await kbnInternalStatusRequest.json();
+      const promMetrics = formatter(kbnMetrics);
+
+      return response.ok({
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: promMetrics,
+      });
+    }
+  );
+}
